4b268e908909b16470ddac0e9f9db6957ad87742
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:25 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Akeyless SRA demo environment with Docker Compose, Crossplane, and cert-manager";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ kubectl kustomize docker-compose ];
+      };
+    });
+}
